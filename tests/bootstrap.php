b6dff74fc97b3f69e5fee1bee3e7e8026564a33e
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-05-10 13:58:55 +0300

    add inflectot to bootstrap container

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,15 @@
 <?php
 
+use Nip\Container\Container;
+use Nip\Inflector\Inflector;
+
 require dirname(__DIR__).'/vendor/autoload.php';
 
-define('PROJECT_BASE_PATH', __DIR__ . '/..');
+define('PROJECT_BASE_PATH', __DIR__.'/..');
 define('TEST_BASE_PATH', __DIR__);
-define('TEST_FIXTURE_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures');
+define('TEST_FIXTURE_PATH', __DIR__.DIRECTORY_SEPARATOR.'fixtures');
+
+$container = new Container();
+$container->set('inflector', new Inflector());
+
+Container::setInstance($container);
